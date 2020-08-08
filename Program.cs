a0d3e2265fc6987f8e76a3f4223b26998c75c6a2
iuri-pdista <correaiuri86@gmail.com>
2020-08-08 16:11:32 -0300

    Testing

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Threading.Tasks;
-using Refit;
 using type_test.Controllers;
 
 namespace type_test
@@ -9,7 +8,26 @@ namespace type_test
     {
         static async Task Main(string[] args)
         {
+            Console.Clear();
+            string apiQuote = await QuoteShow.showQuote();
+            int quoteLength = apiQuote.Length;
+            var splitQuote = apiQuote.Split(" ");
+            string userTry = Console.ReadLine().ToString();
+            int userTryLength = userTry.Length;
+            var userTrySplit = userTry.Split(" ");
+            int count = 0;
+            foreach( string quoteWords in splitQuote ) {
+                foreach( string userTryWords in userTrySplit ){
+                    if( quoteWords == userTryWords ){
+                        count += 1;
+                    }
+                }
+            }
+            Console.WriteLine($"{count} {quoteLength}");
+
+            if (count == quoteLength) Console.WriteLine("Congrats you nailed"); 
             
+            else Console.WriteLine("You failed, need more training"); 
         }
     }
 }
