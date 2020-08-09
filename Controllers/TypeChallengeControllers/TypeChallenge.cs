d03b4026587d27d75070a36677b9de4a7cd380d0
iuri-pdista <correaiuri86@gmail.com>
2020-08-09 18:05:21 -0300

    Fixed number of right words bug :bug:

--- Controllers/TypeChallengeControllers/TypeChallenge.cs
+++ Controllers/TypeChallengeControllers/TypeChallenge.cs
@@ -3,35 +3,37 @@ using type_test.Models;
 
 namespace type_test.Controllers{
     public static class TypeChallenge{
-        public static DateTime RecordTry (string quote) {
-            DateTime end = new DateTime();
-            end = DateTime.Now;
+        public static void RecordTry (string quote) {
             var splitQuote = quote.Split(" ");
             int quoteLength = splitQuote.Length;
             string userTry = Console.ReadLine().ToString();
             var userTrySplit = userTry.Split(" ");
             int userTryLength = userTrySplit.Length;
-            bool isUserCorrect = ProccessTry(splitQuote, userTrySplit);
+            int count = ProccessTry(splitQuote, userTrySplit);
+            bool isUserCorrect = IsUserCorrect(count, quoteLength);
             TypeChallengeModel.SetNewTry(quote, userTry, isUserCorrect);
-            ResultStats results = new ResultStats(quoteLength, userTryLength);
+            ResultStats results = new ResultStats(quoteLength, count);
             results.ShowResults();
-            return end;
         }
-        private static bool ProccessTry (string[] splitQuote, string[] userTrySplit) {
-            decimal count = 0;
-            foreach ( string quoteWords in splitQuote ) {
-                foreach ( string userTryWords in userTrySplit ){
-                    if ( quoteWords == userTryWords ){
-                        count += 1;
-                    }
+        private static int ProccessTry (string[] splitQuote, string[] userTrySplit) {
+            int count = 0;
+            Console.WriteLine(splitQuote.Length);
+            for (int i = 0; i < splitQuote.Length; i++){
+                if (splitQuote[i] == userTrySplit[i]){
+                    count += 1;
+                    Console.WriteLine(count);
                 }
             }
-            if ( count >= (splitQuote.Length)/2 ) {
+            return count;
+        }
+
+        private static bool IsUserCorrect(int count, int splitQuoteLength){
+            if ( (count >= (splitQuoteLength)/2) && (count <= splitQuoteLength) ) {
                 Console.WriteLine("Congrats you nailed");
                 return true;
             }
             else{
-                Console.WriteLine("You failed, need more training");
+                Console.WriteLine( "You failed, need more training" );
                 return false;
             }
         }
