9c6d4677e15f515f3ba06aa117c51914c8e6a1f5
iuri-pdista <correaiuri86@gmail.com>
2020-08-09 17:44:21 -0300

    Removed unnecessary tryCatchs

--- Controllers/ResultStats/ResultStats.cs
+++ Controllers/ResultStats/ResultStats.cs
@@ -14,28 +14,17 @@ namespace type_test.Controllers {
         
         // todo implement cronometer
         
-        public void ShowResults (){
-            try{
-                List<decimal> listOfResults = this.GetResults();
-                Console.WriteLine($"The quote had: {this.quoteSize} words\nand you typed correctly: {this.numberOfRightWords}");
-                Console.WriteLine($"Therefore your accuracy was: {listOfResults[0]}%");
-            }
-            catch (Exception ex){
-                Console.WriteLine(ex);
-            }
+        public void ShowResults (){            
+            List<decimal> listOfResults = this.GetResults();
+            Console.WriteLine($"The quote had: {this.quoteSize} words\nand you typed correctly: {this.numberOfRightWords}");
+            Console.WriteLine($"Therefore your accuracy was: {listOfResults[0]}%");
         }
 
         private List<decimal> GetResults () {
-            try{
-                List<decimal> resultsList = new List<decimal>();
-                decimal accuracy = this.GetAccuracy(this.numberOfRightWords, this.quoteSize);
-                resultsList.Add(accuracy);
-                return resultsList;
-            }
-            catch(Exception ex){
-                Console.WriteLine(ex);
-                return null;
-            }
+            List<decimal> resultsList = new List<decimal>();
+            decimal accuracy = this.GetAccuracy(this.numberOfRightWords, this.quoteSize);
+            resultsList.Add(accuracy);
+            return resultsList;
         }
 
         private decimal GetAccuracy(int totalRightWords, int sizeQuote){
