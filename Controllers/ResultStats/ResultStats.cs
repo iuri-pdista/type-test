8e72315c98aaec619535e7e94dcef59dcd493ad3
iuri-pdista <correaiuri86@gmail.com>
2020-08-11 13:26:43 -0300

    Fixed Accuracy bug :bug:

--- Controllers/ResultStats/ResultStats.cs
+++ Controllers/ResultStats/ResultStats.cs
@@ -3,33 +3,33 @@ using System;
 
 namespace type_test.Controllers {
     public class ResultStats{
-        public ResultStats (int QuoteSize, int NumberOfRightWords){
+        public ResultStats (double QuoteSize, double NumberOfRightWords){
             this.quoteSize = QuoteSize;
             this.numberOfRightWords = NumberOfRightWords;
         }
         public ResultStats (){ }
-        int quoteSize;
-        int numberOfRightWords;
+        double quoteSize;
+        double numberOfRightWords;
         TimeSpan totalElapsedTime;
         
         // todo implement cronometer
         
         public void ShowResults (){            
-            List<decimal> listOfResults = this.GetResults();
-            Console.WriteLine($"The quote had: {this.quoteSize} words\nand you typed correctly: {this.numberOfRightWords}");
-            Console.WriteLine($"Therefore your accuracy was: {listOfResults[0]}%");
+            List<double> listOfResults = this.GetResults();
+            Console.WriteLine($"The quote had: {this.quoteSize} words\nAnd you typed correctly: {this.numberOfRightWords}");
+            Console.WriteLine($"Therefore your accuracy was: {String.Format("{0:0.00}",listOfResults[0])}%");
         }
 
-        private List<decimal> GetResults () {
-            List<decimal> resultsList = new List<decimal>();
-            decimal accuracy = this.GetAccuracy(this.numberOfRightWords, this.quoteSize);
+        private List<double> GetResults () {
+            List<double> resultsList = new List<double>();
+            double accuracy = this.GetAccuracy(this.numberOfRightWords, this.quoteSize);
             resultsList.Add(accuracy);
             return resultsList;
         }
 
-        private decimal GetAccuracy(int totalRightWords, int sizeQuote){
-            decimal accuracy = (totalRightWords / quoteSize) * 100;
-            return accuracy;
+        private double GetAccuracy(double totalRightWords, double sizeQuote){
+            Console.WriteLine(totalRightWords + quoteSize);
+            return ((totalRightWords / quoteSize)*100);
         }
 
         public void GetTotalTime (DateTime start) {
